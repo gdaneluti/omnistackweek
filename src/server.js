19f7fd190bc1bcf72cc5679d3ac45fe2a2f0f23a
Gustavo Daneluti <gdaneluti@e-safer.com.br>
2019-04-12 13:17:08 -0300

    Adiciona Variaveis Ambiente

--- src/server.js
+++ src/server.js
@@ -27,10 +27,9 @@ app.use((req, res, next) => {
     return next();
 });
 
-
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(require('./routes'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
