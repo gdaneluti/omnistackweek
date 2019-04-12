1579399fb779511f568677c539c3f483c565eaff
Gustavo Daneluti <gdaneluti@e-safer.com.br>
2019-04-12 12:09:24 -0300

    Adicao de git ignore

--- src/server.js
+++ src/server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
+
+
 const app = express();
+
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+io.on('connection', socket => {
+    socket.on('connectRoom', box => {
+    });
+        socket.join(box);
+});
 
 mongoose.connect('mongodb://omnistack:omnistack@localhost:27017/omnistack', {
     useNewUrlParser: true
 });
+
+app.use((req, res, next) => {
+    req.io = io;
+    return next();
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(require('./routes'));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 
-app.listen(3333);
+server.listen(3333);
